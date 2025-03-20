68105113c8d5f562b9860613cc1714974d4a71e0
Mohamed Riyaz <frappeteam@syvasoft.in>
2025-03-21 00:56:22 +0530

    purchase receipt button update

--- un_submit/client_script/purchase_receipt.js
+++ un_submit/client_script/purchase_receipt.js
@@ -1,16 +1,23 @@
 frappe.ui.form.on("Purchase Receipt", {
     refresh: function(frm) {
+        // Hide/Show Submit button based on conditions
+        if (frm.doc.docstatus === 0 && frm.doc.ignore_permissions === 1) {
+            frm.page.btn_primary.hide();
+        } 
+
         // When the document is submitted, show "Switch to Draft"
         if (frm.doc.docstatus === 1) {
             let button_label = "Switch to Draft";
             let confirmation_message = __("Are you sure you want to revert this {0} to Draft?", [frm.doc.doctype]);
-            
+
             frm.add_custom_button(button_label, () => {
+                
+                // Save the form after successful submission
+                frm.save_or_update();
                 frappe.warn(
                     confirmation_message,
                     "This action will change the document status.",
                     () => {
-                        // Call revert_docstatus for "Switch to Draft"
                         frappe.call({
                             method: "un_submit.utils.revert_docstatus.revert_docstatus",
                             args: {
@@ -38,13 +45,15 @@ frappe.ui.form.on("Purchase Receipt", {
         else if (frm.doc.docstatus === 0 && frm.doc.ignore_permissions === 1) {
             let button_label = "Switch to Submit";
             let confirmation_message = __("Are you sure you want to submit this {0}?", [frm.doc.doctype]);
-            
+
             frm.add_custom_button(button_label, () => {
+                
+                // Save the form after successful submission
+                frm.save_or_update();
                 frappe.warn(
                     confirmation_message,
                     "This action will change the document status.",
                     () => {
-                        // First, call revert_docstatus (common for both actions)
                         frappe.call({
                             method: "un_submit.utils.revert_docstatus.revert_docstatus",
                             args: {
@@ -53,7 +62,6 @@ frappe.ui.form.on("Purchase Receipt", {
                             },
                             callback: function(response) {
                                 if (!response.exc) {
-                                    // Then, call after_submit_purchase_receipt with a plain JS object and method flag
                                     frappe.call({
                                         method: "un_submit.server_script.purchase_receipt_override.after_submit_purchase_receipt",
                                         args: {
@@ -66,6 +74,7 @@ frappe.ui.form.on("Purchase Receipt", {
                                                     message: __("Document has been successfully submitted."),
                                                     indicator: "green"
                                                 });
+                                                
                                                 frappe.ui.toolbar.clear_cache();
                                                 frm.reload_doc();
                                             }
