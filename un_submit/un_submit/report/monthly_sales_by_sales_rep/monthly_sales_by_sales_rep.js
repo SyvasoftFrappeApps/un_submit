1640251c4b308601e76709a2b173cde0cacec1e7
Balamn <balamn1986@gmail.com>
2025-07-04 17:41:23 +0530

    Chart

--- un_submit/un_submit/report/monthly_sales_by_sales_rep/monthly_sales_by_sales_rep.js
+++ un_submit/un_submit/report/monthly_sales_by_sales_rep/monthly_sales_by_sales_rep.js
@@ -1,13 +1,26 @@
-// Copyright (c) 2025, SyvaSoft Business Solutions Pvt Ltd and contributors
-// For license information, please see license.txt
-
 frappe.query_reports["Monthly Sales by Sales Rep"] = {
-        "filters": [
-					{
+	"filters": [
+		{
 			"fieldname": "month",
-			"label": "Month/Year",
-			"fieldtype": "Link",
-			"options": "month"
+			"label": "Month (YYYY-MM)",
+			"fieldtype": "MultiSelect",
+			"get_data": function (txt) {
+				return frappe.db.get_list('Sales Invoice', {
+					fields: ["posting_date"],
+					filters: {
+						docstatus: 1
+					},
+					limit: 1000
+				}).then(res => {
+					let months = new Set();
+					res.forEach(r => {
+						let date = frappe.datetime.str_to_obj(r.posting_date);
+						let formatted = frappe.datetime.obj_to_str(new Date(date.getFullYear(), date.getMonth(), 1)).slice(0, 7);
+						months.add(formatted);
+					});
+					return [...months].sort().map(m => ({ value: m, label: m }));
+				});
+			}
 		}
-        ]
+	]
 };
--- un_submit/un_submit/report/monthly_sales_by_sales_rep/monthly_sales_by_sales_rep.py
+++ un_submit/un_submit/report/monthly_sales_by_sales_rep/monthly_sales_by_sales_rep.py
@@ -1,22 +1,27 @@
 import frappe
 
 def execute(filters=None):
+    filters = filters or {}
+
     columns = [
         {"label": "Month", "fieldname": "month", "fieldtype": "Data", "width": 100},
         {"label": "Sales Rep", "fieldname": "sales_person", "fieldtype": "Data", "width": 150},
         {"label": "Total Sales", "fieldname": "total", "fieldtype": "Currency", "width": 120},
     ]
 
+    data = get_data(filters)
+    return columns, data
+
+def get_data(filters):
     conditions = ""
     values = {}
 
-    if filters:
-        if filters.get("month"):
-            month = tuple(filters["month"])
-            conditions += " AND DATE_FORMAT(si.posting_date, '%Y-%m') IN %(month)s"
-            values["month"] = month
+    if filters.get("month"):
+        months = tuple(filters.get("month"))
+        conditions += " AND DATE_FORMAT(si.posting_date, '%%Y-%%m') IN %(months)s"
+        values["months"] = months
 
-    data = frappe.db.sql(f"""
+    return frappe.db.sql("""
         SELECT 
             DATE_FORMAT(si.posting_date, '%%Y-%%m') AS month,
             sp.sales_person AS sales_person,
@@ -26,6 +31,4 @@ def execute(filters=None):
         WHERE si.docstatus = 1 {conditions}
         GROUP BY month, sales_person
         ORDER BY month DESC, sales_person ASC
-    """, values, as_dict=True)
-
-    return columns, data
+    """.format(conditions=conditions), values, as_dict=True)
